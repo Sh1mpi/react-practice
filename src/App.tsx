50c682726ae8e180db936073a0a4f9ed96ecd975
Sh1mpi <meqalion2@gmail.com>
2023-12-30 03:46:56 +0300

    pagination 1.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,77 @@
+import React, { useEffect, useState } from 'react';
+import { Table, Button } from 'antd';
+import axios from 'axios';
 import { ColumnsType } from 'antd/es/table';
-import './App.css'
-import { Table } from 'antd';
+import './App.css';
+
+interface DataType {
+  time: string;
+  asset_id_quote: string;
+  rate: number;
+}
+
+const LIMIT = 10;
+const MAX_DATA = 1000;
 
 function App() {
+  const [data, setData] = useState<DataType[]>([]);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [dataSource, setDataSource] = useState<DataType[]>([]);
 
-  interface DataType {
-    key: string;
-    name: string;
-    age: number;
-    address: string;
-    tags: string[];
+  const getExchangeRates = async () => {
+    try {
+      const response = await axios.get('https://rest.coinapi.io/v1/exchangerate/USD', {
+        params: { invert: true },
+        headers: { 'X-CoinAPI-Key': 'E8662ABD-E720-4C34-B9A7-BF8DB18241BF' }
+      });
+      const limitedData = response.data.rates.slice(0, MAX_DATA).map((rate: any) => ({
+        key: rate.asset_id_quote,
+        time: rate.time,
+        asset_id_quote: rate.asset_id_quote,
+        rate: rate.rate
+      }));
+      setData(limitedData);
+    } catch (error) {
+      console.error("Ошибка при получении данных:", error);
+    }
   }
-  
-  // Использование
-  
-  const dataSource: DataType[] = [
-    {
-      key: '1',
-      name: 'Mike',
-      age: 32,
-      address: '10 Downing Street',
-      tags: ['good man']
-    },
-    {
-      key: '2',
-      name: 'John',
-      age: 42,
-      address: '10 Downing Street',
-      tags: ['bad man']
-    },
-  ];
-  
+
+  useEffect(() => {
+    getExchangeRates();
+  }, []);
+
+  useEffect(() => {
+    const start = (currentPage - 1) * LIMIT;
+    const end = start + LIMIT;
+    setDataSource(data.slice(start, end));
+  }, [currentPage, data]);
+
   const columns: ColumnsType<DataType> = [
     {
-      title: 'Name',
-      dataIndex: 'name',
-      key: 'name',
+      title: 'Time',
+      dataIndex: 'time',
+      key: 'time',
     },
     {
-      title: 'Age',
-      dataIndex: 'age',
-      key: 'age',
+      title: 'Asset ID',
+      dataIndex: 'asset_id_quote',
+      key: 'asset_id_quote',
     },
     {
-      title: 'Address',
-      dataIndex: 'address',
-      key: 'address',
-    },
-    {
-      title: 'Tags',
-      dataIndex: 'tags',
-      key: 'tags',
+      title: 'Rate',
+      dataIndex: 'rate',
+      key: 'rate',
     },
   ];
-  
+
   return (
-    <Table dataSource={dataSource} columns={columns} />
+    <>
+      <Table dataSource={dataSource} columns={columns} pagination={false} />
+      <Button onClick={() => setCurrentPage(currentPage - 1)} disabled={currentPage <= 1}>Назад</Button>
+      <Button onClick={() => setCurrentPage(currentPage + 1)} disabled={currentPage * LIMIT >= data.length}>Вперед</Button>
+      <div>Текущая страница: {currentPage}</div>
+    </>
   )
 }
 
-export default App
+export default App;
